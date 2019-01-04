24261e3460183c047f23c4130563b96af5a4df0f
pocopennar <45471905+pocopennar@users.noreply.github.com>
2019-01-04 15:57:18 +0100

    fonction gains()

--- projet_dilemme1.py
+++ projet_dilemme1.py
@@ -17,3 +17,18 @@ def choix():
         print("Merci de n'utiliser que les touche 'a' et 'p' du clavier") 
         choix()
 
+# fonction de gains
+# Prend en paramètre l'action choisie par le joueur, celle du partenaire virtuel et applique en fonction de ces derniers, la matrice des gains pour faire évoluer les scores.       
+def gains(ActionJ, ActionPV, GainC, GainT, PerteT, PerteC) : #ActionJ = action du joueur ; ActionPV = action du PV ; GainC = Gains si les deux partenaires coopèrent ; GainT = Gains du partenaire qui a trahi si l'autre à coopérer ; PerteT = Perte si les deux joueurs ont trahi ; PerteC = Perte du partenaire qui a coopéré quand l'autre a trahi. 
+    if ActionJ== 'C' and ActionPV== 'C' :
+        scoreJ += GainC
+        scorePV += GainC
+    if ActionJ== 'C' and ActionPV== 'T':
+        scoreJ += PerteC
+        scorePV += GainT
+    if ActionJ== 'T' and ActionPV== 'C' :
+        scoreJ += GainT
+        scorePV += PerteC
+    if ActionJ== 'T' and ActionPV== 'T':
+        scoreJ += PerteT
+        scorePV += PerteT

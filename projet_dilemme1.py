9bc86715544194025994ef166fc4fc091c620d81
pocopennar <45471905+pocopennar@users.noreply.github.com>
2019-01-07 17:08:19 +0100

    simple update avancement readme

--- projet_dilemme1.py
+++ projet_dilemme1.py
@@ -2,10 +2,7 @@
 
 # Ce programme vise à simuler un dilemme du prisonnier entre deux joueurs.
 
-scoreJ = 0 #Score du Joueur, nul au début du jeu
-scoreV = 0 #Score du partenaire Virtuel, nul au début du jeu
-
-# fonction choix
+# fonction de choix
 # recueil le choix du joueur entre coopérer et trahir
 def choix():
     action = input("Souhaitez vous coopérer (a) ou trahir (p) :") 
@@ -18,17 +15,44 @@ def choix():
         choix()
 
 # fonction de gains
-# Prend en paramètre l'action choisie par le joueur, celle du partenaire virtuel et applique en fonction de ces derniers, la matrice des gains pour faire évoluer les scores.       
-def gains(ActionJ, ActionPV, GainC, GainT, PerteT, PerteC) : #ActionJ = action du joueur ; ActionPV = action du PV ; GainC = Gains si les deux partenaires coopèrent ; GainT = Gains du partenaire qui a trahi si l'autre à coopérer ; PerteT = Perte si les deux joueurs ont trahi ; PerteC = Perte du partenaire qui a coopéré quand l'autre a trahi. 
+# Prend en paramètre l'action choisie par le joueur, celle du partenaire virtuel et applique en fonction de ces derniers, la matrice des gains.       
+def gains(ActionJ, ActionPV, GainC, GainT, PerteC, PerteT) : #ActionJ = action du joueur ; ActionPV = action du PV ; GainC = Gains si les deux partenaires coopèrent ; GainT = Gains du partenaire qui a trahi si l'autre à coopérer ; PerteT = Perte si les deux joueurs ont trahi ; PerteC = Perte du partenaire qui a coopéré quand l'autre a trahi. 
     if ActionJ== 'C' and ActionPV== 'C' :
-        scoreJ += GainC
-        scorePV += GainC
+        return (GainC, GainC)
     if ActionJ== 'C' and ActionPV== 'T':
-        scoreJ += PerteC
-        scorePV += GainT
+        return (PerteC, GainT)
     if ActionJ== 'T' and ActionPV== 'C' :
-        scoreJ += GainT
-        scorePV += PerteC
+        return (GainT, PerteC)
     if ActionJ== 'T' and ActionPV== 'T':
-        scoreJ += PerteT
-        scorePV += PerteT
+        return (PerteT, PerteT)
+        
+# fonction choix du nombre de tours
+def nbtours() :
+    nbtours = input("Choisir le nombre de tour du jeu (merci de choisir un nombre entier positif) : ")
+    return nbtours
+        
+    
+# bloc de paramétrage     
+print("Vous allez maintenant paramétrer le dilemme du prisonnier.")
+GainC = int(input("Choisir le montant du gain de chaque partenaire si les deux partenaires coopérent : " ))
+GainT =int(input("Choisir le montant du gain du partenaire qui trahit si l'autre coopère : " ))
+PerteC =int(input("Choisir le montant de la perte du partenaire qui coopère si l'autre trahit : " ))
+PerteT =int(input("Choisir le montant de la perte de chaque partenaire si les deux partenaires trahisent : " ))
+Nbtours = int(input("Choisir le nombre de tours de jeu : "))
+scoreJ = 0 #Score du Joueur, nul au début du jeu
+scorePV = 0 #Score du partenaire Virtuel, nul au début du jeu
+n = 0 #indice des tours
+ActionPV = 'C' # Initialisation de l'action du PV : coopère au premier tour
+
+# début du jeu
+while n < Nbtours :
+    n+=1
+    ActionJ = choix()
+    scoreJ+= gains(ActionJ, ActionPV, GainC, GainT, PerteC, PerteT)[0]
+    scorePV += gains(ActionJ, ActionPV, GainC, GainT, PerteC, PerteT)[1]
+    ActionPV = ActionJ
+    print("Score du joueur = ", scoreJ, " Score du partenaire = ", scorePV)
+print("Scores finaux : Joueur : ", scoreJ,"  Partenaire : ", scorePV)
+print("Merci d'avoir participer. L'expérience est terminée, veuillez appeler l'expérimentateur.")
+    
+ 

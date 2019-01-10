0d8ebf05ac2100204bf6207de3bbd6a5f0453d45
pocopennar <45471905+pocopennar@users.noreply.github.com>
2019-01-11 00:31:38 +0100

    Add files via upload

---
+++ projet_dilemme2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Ce programme vise à créer une interface pour paramétrer et jouer au dilemme du prisonnier
+import expyriment
+
+exp = expyriment.design.Experiment(name="Prisoner dilemma")
+expyriment.control.initialize(exp)
+ 
+expyriment.control.start()
+
+
+
+
+expyriment.control.end()
+    
+    
+      
